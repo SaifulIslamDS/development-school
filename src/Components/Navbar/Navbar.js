2d1c829d6ab7ce12c6e26583cbc6c49baeac9c64
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-11-19 23:08:02 +0600

    Update

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import logo from "../../img/logo.png";
 
 const Navbar = () => {
     return (
         <section className="nav-section">
             <div className="container mx-auto ">
                 <div className="grid grid-cols-3 gap-4">
-                    <img className="site-logo" src={logo} alt="" />
+                        <h2 className="text-5xl pt-6 text-yellow-400"><Link to="/">DevSchool</Link> </h2>
                     <div></div>
                     <nav className="navbar">
                         <Link to="/">Home</Link>
